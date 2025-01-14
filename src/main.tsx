b57b82ca300e51f659096b44addc86d9e6596683
Narit <NaritSudprasertS@gmail.com>
2025-01-14 14:06:43 +0700

    update login, register components and login logic(also authcontext)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./index.css"
+import App from "./App.tsx"
+import HomePage from "./pages/HomePage.tsx"
+import MainLayout from "./layouts/MainLayout.tsx"
+import Providers from "./contexts/Providers.tsx"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <Providers>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainLayout />}>
+                        <Route index element={<HomePage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </Providers>
+    </StrictMode>,
 )
